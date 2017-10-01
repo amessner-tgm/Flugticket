ed642fe4692e17bb9400723239773e26fcc67312
amessner <alexander.messner.am@gmail.com>
2017-10-01 16:09:54 +0200

    Input,Output.php in Order hinzugefügt

---
+++ Fluginfo/input.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+</head>
+<body>
+  <form action="output.php" method="get">
+    <input type="text" name="flugnr">
+    <button type="submit">Submit</button>
+  </form>
+</body>
+</html>
