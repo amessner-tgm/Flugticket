d6eef2bc67baf44ea8b7fd13fd69cf5da0c504ae
ygoerguelue-tgm <ygoerguelue@student.tgm.ac.at>
2017-10-05 21:26:53 +0200

    Add files via upload

--- Fluginfo/input.php
+++ Fluginfo/input.php
@@ -1,11 +1,23 @@
 <html>
 <head>
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<title>Flüge</title>
 </head>
 <body>
-  <form action="output.php" method="get">
-    <input type="text" name="flugnr">
-    <button type="submit">Submit</button>
-  </form>
-</body>
+  <center>
+    <div class="page-header">
+      <h1>Flüge<br \><small>Geben Sie bitte Ihre Flugnummer ein.</small></h1>
+    </div>
+  </center>
+  <center>
+    <form class="form-inline" action="output.php" method="get">
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-addon"><i class="glyphicon glyphicon-plane"></i></div>
+          <input type="text" class="form-control" name="flugnr" placeholder="Flugnummer">
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Akzeptieren</button>
+    </form>
+  </center>
 </html>
